4c59ae0d42dd6b309c1e1075ae6a9c76e0cd201b
mathyber <ilan23f@gmail.com>
2025-03-25 18:25:25 +0300

    fix

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const config = require('../config/config');
+const {startBot} = require("../src/bot");
 
 const app = express();
 const PORT = config.PORT;
